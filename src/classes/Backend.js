8a395d76e5f9a6579cc15aab4a54e2293ac57a31
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-09-26 19:49:57 +0200

    Changed baseUrl (Hack for Dev)

--- src/classes/Backend.js
+++ src/classes/Backend.js
@@ -1,51 +1,54 @@
-import axios from "axios";
+import axios from 'axios';
 
 class Backend {
+	static baseUrl = 'https://dev.bergflix.de/backend/'; //A quick hack just for developement
+	loaded = false;
+	loadListeners = [];
 
-    static baseUrl = "/backend/";
-    loaded = false;
-    loadListeners = [];
+	constructor() {
+		axios
+			.get(Backend.baseUrl)
+			.then(() => {
+				this.loaded = true;
+				this.loadListeners.forEach((func) => func.call());
+			})
+			.catch((e) => console.error(e));
+	}
 
-    constructor() {
-        axios.get(Backend.baseUrl).then(() => {
-                this.loaded = true;
-                this.loadListeners.forEach(func => func.call());
-            }).catch(e => console.error(e));
-    }
+	onLoad(func) {
+		if (this.loaded) func.call();
+		else this.loadListeners.push(func);
+	}
 
-    onLoad(func) {
-        if(this.loaded) func.call();
-        else this.loadListeners.push(func);
-    }
+	async get(key = '') {
+		try {
+			return (await axios.get(Backend.baseUrl + 'media/' + key)).data;
+		} catch (err) {
+			return { error: true, response: err };
+		}
+	}
 
-    async get(key = "") {
-        try {
-            return (await axios.get(Backend.baseUrl + "media/" + key)).data;
-        } catch(err) {
-            return {error: true, response: err};
-        }
-    }
+	async getList(type, limit = 0, start = 0) {
+		try {
+			return (await axios.get(Backend.baseUrl + type + '?sort=date' + (limit ? '&limit=' + limit : '') + (start ? '&start=' + start : '')))
+				.data;
+		} catch (err) {
+			return { error: true, response: err };
+		}
+	}
 
-    async getList(type, limit = 0, start = 0) {
-        try {
-            return (await axios.get(Backend.baseUrl + type + "?sort=date" + (limit ? "&limit="+limit : "") + (start ? "&start="+start : ""))).data;
-        } catch(err) {
-            return {error: true, response: err};
-        }
-    }
+	async find(query = {}) {
+		let keys = Object.keys(query);
+		if (!keys.length) return [];
 
-    async find(query = {}) {
-        let keys = Object.keys(query);
-        if(!keys.length) return [];
-
-        let list = [];
-        keys.forEach(prop => query[prop] && list.push(prop + "=" + query[prop]));
-        try {
-            return (await axios.get(Backend.baseUrl + "media?" + list.join("&"))).data;
-        } catch(err) {
-            return {error: true, response: err};
-        }
-    }
+		let list = [];
+		keys.forEach((prop) => query[prop] && list.push(prop + '=' + query[prop]));
+		try {
+			return (await axios.get(Backend.baseUrl + 'media?' + list.join('&'))).data;
+		} catch (err) {
+			return { error: true, response: err };
+		}
+	}
 }
 
 export default new Backend();
