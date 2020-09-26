531bf78a3724f6ab02d5b61bdbe04bfd749a8da7
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2020-09-26 22:23:13 +0200

    Made BG darker

--- src/components/BackgroundImage/index.js
+++ src/components/BackgroundImage/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import "./style.scss";
-import {TransitionGroup, CSSTransition} from "react-transition-group";
-import {v4 as uuid} from "uuid";
+import './style.scss';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { v4 as uuid } from 'uuid';
 
 class BackgroundImage extends React.Component {
-    render() {
-        return (
-            <TransitionGroup id={"background-container"}>
-                <CSSTransition appear={true} key={uuid()} timeout={450} classNames={"background"}>
-                    <React.Fragment>
-                        <span className={"background"} style={{backgroundImage:  `url(${this.props.image})`}} />
-                        <span className={"background"} />
-                    </React.Fragment>
-                </CSSTransition>
-            </TransitionGroup>
-        );
-    }
+	render() {
+		return (
+			<TransitionGroup id={'background-container'}>
+				<CSSTransition appear={true} key={uuid()} timeout={450} classNames={'background'}>
+					<React.Fragment>
+						<span className={'background'} style={{ backgroundImage: `url(${this.props.image})`, opacity: 0.6 }} />
+						<span className={'background'} />
+					</React.Fragment>
+				</CSSTransition>
+			</TransitionGroup>
+		);
+	}
 }
 
 export default BackgroundImage;
